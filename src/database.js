665a06f28076a005b422ade664d2d4c2e6319e9e
Easton Smith <eastonsmith37@gmail.com>
2023-07-14 00:35:58 -0400

    updated sql functions

--- src/database.js
+++ src/database.js
@@ -18,43 +18,51 @@ module.exports = con;
 function addCard(data){
     var sql = `INSERT INTO credit_cards 
         (number, expiry_date, security_num, active) 
-        VALUES (true)`;
+        VALUES (${data.number}, ${data.expiry_date}, ${data.security_num}, true)`;
     runQry(sql);
 }
 
 function addUser(data){
     var sql = `INSERT INTO users 
         (f_name, l_name, email, phone, active) 
-        VALUES (true)`;
+        VALUES (${data.f_name}, ${data.l_name}, ${data.email}, ${data.phone}, true)`;
     runQry(sql);
 }
 
 function signOutCard(data){
     var sql = `INSERT INTO sign_out
         (card_id, user_id, date, expected_return) 
-        VALUES ()`;
+        VALUES (${data.card_id}, ${data.user_id}, ${data.date}, ${data.expected_return})`;
     runQry(sql);
 }
 
 function deleteCard(data){
     var sql = `DELETE FROM credit_cards 
-        WHERE number=12 AND security_num=123`;
+        WHERE number=${data.number} AND security_num=${data.security_num}`;
     runQry(sql);
 }
 
 function deleteUser(data){
     var sql = `DELETE FROM users
-        WHERE f_name=name AND email=email`;
+        WHERE f_name=${data.f_name} AND email=${data.email}`;
     runQry(sql);
 }
 
 function returnCard(data){
-    
+    var sql = `SELECT expected_return FROM sign_out 
+    WHERE card_id=${data.card_id} AND user_id=${data.user_id}`
 
-    var sql = `UPDATE sign_out 
-        SET actual_return=date, returned_on_time=true
-        WHERE card_id=id AND user_id=id`;
-    runQry(sql);
+    db.query(sql, (err, returnData, fields) => {
+        if(err) throw err;
+
+        // check date before after
+
+        sql = `UPDATE sign_out 
+        SET actual_return=${data.actual_return}, returned_on_time=true
+        WHERE card_id=${data.card_id} AND user_id=${data.user_id}`;
+
+        runQry(sql);
+    });
 }
 
 function runQry(sql){
