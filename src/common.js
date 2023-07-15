33b6c859bc21f8bcd4386cc8282d71eae5bbf59d
Easton Smith <eastonsmith37@gmail.com>
2023-07-15 15:50:25 -0400

    fixed insert rows

--- src/common.js
+++ src/common.js
@@ -1,8 +1,8 @@
 function addRow(id, data){
     var table = document.getElementById(id);
-    var row = table.insertRow(1);
-    var i=0;
     data.forEach(element => {
+        var row = table.insertRow(1);
+        var i=0;
         for(const key in element){
             var col = row.insertCell(i);
             col.innerHTML = element[key];
